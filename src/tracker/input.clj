814604bb13f34fcb40d3ac2c1e15654a8a9a58ca
Janne <jlemmetti@gmail.com>
2013-01-18 22:28:33 +0200

    Tweaked docstring

--- src/tracker/input.clj
+++ src/tracker/input.clj
@@ -1,6 +1,6 @@
 (ns tracker.input)
 
 (defn read-time
-  "Parses input string representing time into a number representing the given time in minutes. Currently only supports strings in the form '#m' and '#' where # is an integer number"
+  "Parses input string representing time into a number representing the given time in minutes. Currently only supports strings in the form '#m' and '#' where # is an integer value"
   [input]
   (Integer/valueOf (re-find #"\d+" input)))
