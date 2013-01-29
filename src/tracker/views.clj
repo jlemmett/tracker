4422a983574a19c621379dbf8283ef7173356a6f
Janne <jlemmetti@gmail.com>
2013-01-29 22:04:35 +0200

    Separated the task row template into a snippet

--- src/tracker/views.clj
+++ src/tracker/views.clj
@@ -2,30 +2,32 @@
   (:require [net.cgrand.enlive-html :as h]))
 
 ;; TODO: Use this instead of the embedded row in task-input.html
-(def task-row (h/html-resource "resources/task-row-snippet.html"))
+;(def task-row (h/html-resource "resources/task-row-snippet.html"))
 
-;; The task input view: populates the work categories element with provided category names and shows
-;; any existing tasks
-(h/deftemplate task-input "resources/task-input.html"
-  [categories existing-tasks]
-  [:select#work-category :option] (h/clone-for [c categories]
-                                               (h/do->
-                                                (h/content c)
-                                                (h/set-attr :value c)))
+(h/defsnippet task-row "resources/task-row-snippet.html" [:tr.task-row]
+             [tasks]
 
-  [:#existing-tasks :table :tr.task-row]
-  (h/clone-for [task existing-tasks]
-               [:td.time]
-               (h/content (:time task))
+             [:tr.task-row]
+             (h/clone-for [task tasks]
+                          [:td.time]
+                          (h/content (:time task))
 
-               [:td.work-category]
-               (h/content (str (:work-category task)))
+                          [:td.work-category]
+                          (h/content (str (:work-category task)))
 
-               [:td.tags]
-               (h/content (:tags task))
+                          [:td.tags]
+                          (h/content (:tags task))
 
-               [:td.description]
-               (h/content (:description task))
+                          [:td.description]
+                          (h/content (:description task))))
 
-               )
-  )
+  ;; The task input view: populates the work categories element with provided category names and shows
+  ;; any existing tasks
+  (h/deftemplate task-input "resources/task-input.html"
+    [categories existing-tasks]
+    [:select#work-category :option] (h/clone-for [c categories]
+                                                 (h/do->
+                                                  (h/content c)
+                                                  (h/set-attr :value c)))
+
+    [:#existing-tasks :table] (h/content (task-row existing-tasks)))
